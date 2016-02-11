761f58730ac5f1bbbe41c61423dc2afd2890a4bd
Harold C <hcabalic@gmail.com>
2016-02-11 01:24:03 -0800

    Update EspressoBar.js

--- expressoBar/build/EspressoBar.js
+++ expressoBar/build/EspressoBar.js
@@ -11,7 +11,7 @@ var EspressoBar = React.createClass({displayName: "EspressoBar",
     // we need to call shuffle(); on each specific reel
     // otherwise items will shuffle to other reels
     for (var i = 1; i < lists.length + 1; i++){
-      $('ul#list' + i + ' li').shuffle();
+      $('ul#reel' + i + ' li').shuffle();
     }
 
     // check winner after every spin
@@ -41,32 +41,34 @@ var EspressoBar = React.createClass({displayName: "EspressoBar",
     alert('you won a ' + str);
   },
 
+  // TODO: Improve
+  // render eachReel()
+  // different property names?
   render: function() {
     return (
       React.createElement("div", {className: "container"}, 
-        React.createElement("div", {className: "col-md-4"}, 
-          React.createElement("ul", {id: "list1", className: "list-unstyled"}, 
-            React.createElement("li", {className: "coffee"}, "coffee maker"), 
-            React.createElement("li", {className: "tea"}, "teapot"), 
-            React.createElement("li", {className: "espresso"}, "espresso machine")
-          )
-        ), 
-        React.createElement("div", {className: "col-md-4"}, 
-          React.createElement("ul", {id: "list2", className: "list-unstyled"}, 
-            React.createElement("li", {className: "coffee"}, "coffee filter"), 
-            React.createElement("li", {className: "tea"}, "tea strainer"), 
-            React.createElement("li", {className: "espresso"}, "espresso tamper")
-          )
-        ), 
-        React.createElement("div", {className: "col-md-4"}, 
-          React.createElement("ul", {id: "list3", className: "list-unstyled"}, 
-            React.createElement("li", {className: "coffee"}, "coffee grounds"), 
-            React.createElement("li", {className: "tea"}, "loose tea"), 
-            React.createElement("li", {className: "espresso"}, "espresso beans")
-          )
-        ), 
-
+        React.createElement(Reel, {id: "reel1", coffee: "coffee maker", tea: "teapot", espresso: "espresso machine"}), 
+        React.createElement(Reel, {id: "reel2", coffee: "coffee filter", tea: "tea strainer", espresso: "espresso tamper"}), 
+        React.createElement(Reel, {id: "reel3", coffee: "coffee grounds", tea: "loose tea", espresso: "espresso beans"}), 
         React.createElement("button", {onClick: this.play, className: "btn btn-primary"}, "Play!")
+      )
+    )
+  }
+});
+
+// TODO: Improve
+// create Block component
+// render eachBlock()
+// different property names?
+var Reel = React.createClass({displayName: "Reel",
+  render: function() {
+    return (
+      React.createElement("div", {className: "col-md-4"}, 
+        React.createElement("ul", {id: this.props.id, className: "list-unstyled"}, 
+          React.createElement("li", {className: "coffee"}, this.props.coffee), 
+          React.createElement("li", {className: "tea"}, this.props.tea), 
+          React.createElement("li", {className: "espresso"}, this.props.espresso)
+        )
       )
     )
   }
