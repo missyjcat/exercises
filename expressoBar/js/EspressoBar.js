b3970e71a2bc4801b8d6fad91dfe53c08752f4a9
Harold C <hcabalic@gmail.com>
2016-02-11 01:23:16 -0800

    Update EspressoBar.js

--- expressoBar/js/EspressoBar.js
+++ expressoBar/js/EspressoBar.js
@@ -11,7 +11,7 @@ var EspressoBar = React.createClass({
     // we need to call shuffle(); on each specific reel
     // otherwise items will shuffle to other reels
     for (var i = 1; i < lists.length + 1; i++){
-      $('ul#list' + i + ' li').shuffle();
+      $('ul#reel' + i + ' li').shuffle();
     }
 
     // check winner after every spin
@@ -41,32 +41,34 @@ var EspressoBar = React.createClass({
     alert('you won a ' + str);
   },
 
+  // TODO: Improve
+  // render eachReel()
+  // different property names?
   render: function() {
     return (
       <div className="container">
-        <div className="col-md-4">
-          <ul id="list1" className="list-unstyled">
-            <li className="coffee">coffee maker</li>
-            <li className="tea">teapot</li>
-            <li className="espresso">espresso machine</li>
-          </ul>
-        </div>
-        <div className="col-md-4">
-          <ul id="list2" className="list-unstyled">
-            <li className="coffee">coffee filter</li>
-            <li className="tea">tea strainer</li>
-            <li className="espresso">espresso tamper</li>
-          </ul>
-        </div>
-        <div className="col-md-4">
-          <ul id="list3" className="list-unstyled">
-            <li className="coffee">coffee grounds</li>
-            <li className="tea">loose tea</li>
-            <li className="espresso">espresso beans</li>
-          </ul>
-        </div>
-
+        <Reel id="reel1" coffee="coffee maker" tea="teapot" espresso="espresso machine" />
+        <Reel id="reel2" coffee="coffee filter" tea="tea strainer" espresso="espresso tamper" />
+        <Reel id="reel3" coffee="coffee grounds" tea="loose tea" espresso="espresso beans" />
         <button onClick={this.play} className="btn btn-primary">Play!</button>
+      </div>
+    )
+  }
+});
+
+// TODO: Improve
+// create Block component
+// render eachBlock()
+// different property names?
+var Reel = React.createClass({
+  render: function() {
+    return (
+      <div className="col-md-4">
+        <ul id={this.props.id} className="list-unstyled">
+          <li className="coffee">{this.props.coffee}</li>
+          <li className="tea">{this.props.tea}</li>
+          <li className="espresso">{this.props.espresso}</li>
+        </ul>
       </div>
     )
   }
